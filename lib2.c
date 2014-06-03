b60e90e69830c22bf49bcdc98e1e7a45b711cc7e
alex <alex@nonari.com>
2014-06-03 12:55:16 +0200

    Merge de newbranch

--- lib2.c
+++ lib2.c
@@ -7,15 +7,15 @@ int sum_up (int *v, int size){
 	return total;
 }
 
-<<<<<<< HEAD
+
 void map_sum(int *v, int size, int a){
 	int i = 0;
 	for (i=0;i<size;i++){
 		v[i] += a;
 	}
-=======
+
 int get(int *v, int p){
 	int b = v[p];
 	return b; 
->>>>>>> newbranch
+
 }
--- lib2.h
+++ lib2.h
@@ -1,6 +1,6 @@
 int sum_up (int *v, int size);
-<<<<<<< HEAD
+
 void map_sum (int *v, int size, int a);
-=======
+
 int get(int *v, int p);
->>>>>>> newbranch
+
