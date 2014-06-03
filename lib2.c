6d4181d09cdc6deed37dc3217acaf9711fa795c8
alex <alex@nonari.com>
2014-06-03 12:48:42 +0200

    Añadida funcion map_sum

--- lib2.c
+++ lib2.c
@@ -6,3 +6,10 @@ int sum_up (int *v, int size){
 	}
 	return total;
 }
+
+void map_sum(int *v, int size, int a){
+	int i = 0;
+	for (i=0;i<size;i++){
+		v[i] += a;
+	}
+}
--- lib2.h
+++ lib2.h
@@ -1,1 +1,2 @@
 int sum_up (int *v, int size);
+void map_sum (int *v, int size, int a);
